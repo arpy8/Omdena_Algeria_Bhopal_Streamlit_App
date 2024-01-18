fbebaed447660085a1d6060661ebc3ab7d394945
Arpit <arpitsengar99@gmail.com>
2024-01-19 02:31:35 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,116 @@
+CSS = open("assets/styles.css", 'r').read()
+
+IMG_BANNER = "assets/banner.png"
+IMG_OMDENA = "assets/omdena-logo.png"
+IMG_LOGO = "assets/logo.png"
+
+SIDEBAR_TEXT_1 = """
+<center><h1>Omdena<br> Algeria & Bhopal<br>Chapter</h1></center>
+"""
+SIDEBAR_TEXT_2 = """
+<hr>
+<h3 class='sidebar_h1'>Disclaimer</h3>
+<h5 class='sidebar_h1'>The predictions provided by this system are for informational purposes only.</h5>
+"""
+
+
+PROJECT_BACKGROUND = """
+## Project background
+The arid climate of several Algerian regions and the water-stressed region of Bhopal, effective water management and forecasting are crucial. This project aims to harness the power of machine learning to address the unique water resource challenges faced by both regions. By creating an open-source solution, we aim to empower Algeria and Bhopal to make informed decisions, optimize resource allocation, and build resilient water infrastructure for a sustainable future. <br><br>
+Local Chapter Website : [Visit Site](https://www.omdena.com/chapter-challenges/open-source-water-management-and-forecasting-project-in-algeria-and-bhopal) <br>
+Dagshub Repository : [Visit Site](https://dagshub.com/Omdena/AlgeriaBhopal_WaterManagementandForecasting) <br>
+"""
+
+PROJECT_PROBLEM = f"""
+## The Problem
+The different water resource challenges faced by both Algeria and Bhopal region in India.
+"""
+
+PROJECT_GOALS = """
+## Project Goals
+##### **1. Develop a Comprehensive Open-Source Water Management and Forecasting System:**
+Create a user-friendly platform tailored to the specific needs of Algeria and Bhopal, integrating machine learning algorithms for precise water forecasting and efficient water resource management. 
+##### **2. Enhance Water Resource Utilization:**
+Improve the sustainable use of water resources in both regions by providing accurate forecasts and real-time monitoring.
+##### **3. Capacity Building:**
+Empower local stakeholders in Algeria and Bhopal with the knowledge and tools necessary to make well-informed decisions about water management.
+##### **4. Community Engagement:**
+Foster collaboration among local government agencies, NGOs, and the research community to collectively address water-related challenges in both regions.
+"""
+
+PROJECT_TIMELINE = """
+## Project Timeline
+##### 1. Project Initiation: 
+Define project goals and scope. Identify key stakeholders and project team members and teams. Secure resources.
+##### 2. Research and Data Collection: 
+Gather historical weather and water data specific to Algeria and Bhopal. Collect satellite imagery and remote sensing data. Use open source data.
+##### 3. Visualization and Exploration:
+Visualize and explore the data and identify relevant machine learning algorithms accordingly.
+##### 4. Development of Water Management Platform: 
+Design and develop the open-source platform for water management and forecasting. Implement machine learning models for water forecasting, water resources disponibility, quality and risks.
+##### 5. Testing and Validation: 
+Conduct extensive testing of the platform using historical data. Validate the accuracy of machine learning models. Address any issues or bugs that arise during testing.
+##### 6. Deployment and Maintenance: 
+Deploy the water management platform for operational use. Monitor and maintain the platform. Provide ongoing support and updates.
+##### 7. Data Dissemination:
+Refine the github repository of the project and prepare the final demo for presentation on the official platforms of Omdena and the parties involved. Write conference and/or journal papers to publish the presented solutions.
+"""
+
+
+ABOUT_US = """
+<h4 style="text-align: center; color:#FFF6F4 !important;">{More info about the problem statement}</h4><hr>
+"""
+
+
+CONTRIBUTORS = """  
+<h1 style="text-align: center; color:#FFF6F4;">A heartfelt thankyou to all our contributors ❤️</h1><hr>
+<div style="text-align:center;">
+<table>
+<tr>
+    <th width="20%" style="font-size: 140%;">Chapter Name</th>    
+    <th width="20%" style="font-size: 140%;">Chapter Lead</th>    
+</tr>
+<tr>
+    <td>Omdena Algeria Chapter</td>    
+    <td>Chapter Lead</td>    
+</tr>
+<tr>
+    <td>Omdena Bhopal Chapter</td>    
+    <td>Chapter Lead</td>    
+</tr>
+</table>
+<br>
+<table>
+    <tbody>
+        <tr>
+            <th width="20%" style="font-size: 140%;" colspan="3">Contributors</th>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>John Doe</td>
+            <td>John Doe</td>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>John Doe</td>
+            <td>John Doe</td>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>John Doe</td>
+            <td>John Doe</td>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>John Doe</td>
+            <td>John Doe</td>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>John Doe</td>
+            <td>John Doe</td>
+        </tr>
+    </tbody>
+</table>
+</div>
+"""
