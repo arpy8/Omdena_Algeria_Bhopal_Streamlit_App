1913d063fcbb57eebb5f704e8f0f78e9d7e85e69
argishh <abhangiargish@gmail.com>
2024-02-10 20:58:08 +0530

    added dated prediction feature

--- model.py
+++ model.py
@@ -3,9 +3,12 @@ import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 from plotly import graph_objects as go
+from datetime import datetime, date
 
 from utils import *
 from config import *
+
+CUR_DATE = datetime.now().date()
 
 def load_data(file_path):
     ds_name = file_path
@@ -34,41 +37,67 @@ def load_model(selected):
     model = joblib.load(model_path)
     return model
 
-
 def main():
     # try:
-        st.write('<center><h1>Forecast Water Availability</h1></center><br>', unsafe_allow_html=True)
-        
-        selected = option_menu(
-            menu_title=None,
-            options=["Algeria", "Bhopal"],
-            icons=["water", "water"],
-            styles=CSS2,
-            orientation="horizontal"
-        )
-        
-        data = load_data(ALGERIA_AGG_DATASET if selected=="Algeria" else BHOPAL_AGG_DATASET)
-        population_data = load_data(ALGERIA_POPULATION if selected=="Algeria" else BHOPAL_POPULATION)
-        population_data.date = pd.to_datetime(population_data.date)
+    st.write('<center><h1>Forecast Water Availability</h1></center><br>', unsafe_allow_html=True)
+    
+    selected = option_menu(
+        menu_title=None,
+        options=["Algeria", "Bhopal"],
+        icons=["water", "water"],
+        styles=CSS2,
+        orientation="horizontal"
+    )
+    
+    data = load_data(ALGERIA_AGG_DATASET if selected=="Algeria" else BHOPAL_AGG_DATASET)
+    population_data = load_data(ALGERIA_POPULATION if selected=="Algeria" else BHOPAL_POPULATION)
+    population_data.date = pd.to_datetime(population_data.date)
 
-        model = load_model(selected)
-        number_input = st.slider("Number of years to forecast", 1, 10, 1)
+    model = load_model(selected)
 
-        forecast_button = st.empty()
-        
-        if forecast_button.button("Forecast"):
-            df_forecast = recursive_multi_step_forecasting_monthly(data, TARGET, model, 365*int(number_input))
-            df_and_forecast = pd.concat([data, df_forecast], axis=0)
+    mode = option_menu(
+        menu_title=None,
+        options=["Yearly", "Dated"],
+        styles=CSS2,
+        orientation="horizontal"
+    )
+    
+    if mode == "Dated":
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            day = st.number_input('Day', min_value=1, max_value=31, value=datetime.now().day)
+        with col2:
+            month = st.number_input('Month', min_value=1, max_value=12, value=datetime.now().month)
+        with col3:
+            year = st.number_input('Year', min_value=1900, max_value=2262, value=datetime.now().year)
+        try:
+            pred_date = date(year, month, day)
+            delta = pred_date - CUR_DATE
+            number_input = delta.days
             
-            df_and_forecast['population'] = [population_data[population_data.date.dt.year == y][' Population'].values[0] for y in df_and_forecast.index.year]
-            daily_water_demand_lcd = 200
-            daily_water_demand_mld = daily_water_demand_lcd * df_and_forecast['population']*10**(-6)
-            df_and_forecast['water_availability'] = df_and_forecast.daily_water_volume - daily_water_demand_mld
+        except ValueError as e:
+            st.error(f"Invalid date: {e}")
 
-            plot_data(df_and_forecast, place=selected ,year=number_input)
-            st.balloons()
-                
-            forecast_button.empty()
+    if mode == "Yearly":    
+        num_days_input = st.slider("Number of years to forecast", 1, 10, 1)
+        number_input = 365*int(num_days_input)
+
+    forecast_button = st.empty()
+    
+    if forecast_button.button("Forecast"):
+        df_forecast = recursive_multi_step_forecasting_monthly(data, TARGET, model, number_input)
+        df_and_forecast = pd.concat([data, df_forecast], axis=0)
+        
+        df_and_forecast['population'] = [population_data[population_data.date.dt.year == y][' Population'].values[0] for y in df_and_forecast.index.year]
+        daily_water_demand_lcd = 200
+        daily_water_demand_mld = daily_water_demand_lcd * df_and_forecast['population']*10**(-6)
+        df_and_forecast['water_availability'] = df_and_forecast.daily_water_volume - daily_water_demand_mld
+
+        plot_data(df_and_forecast, place=selected ,year=number_input)
+        st.balloons()
+            
+        forecast_button.empty()
         
     # except Exception as e:
     #     st.error(f"{e}")
